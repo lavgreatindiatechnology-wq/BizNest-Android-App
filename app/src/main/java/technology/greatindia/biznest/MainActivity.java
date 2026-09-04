41917de7a4ac6712eeee43c7c7f96e0884503d6f
lavgreatindiatechnology-wq <lavgreatindiatechnology@gmail.com>
2026-09-04 08:25:29 +0530

    Update MainActivity.java

--- app/src/main/java/technology/greatindia/biznest/MainActivity.java
+++ app/src/main/java/technology/greatindia/biznest/MainActivity.java
@@ -3,10 +3,8 @@ package technology.greatindia.biznest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
 import android.webkit.CookieManager;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
@@ -14,88 +12,252 @@ import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-import androidx.appcompat.app.AppCompatActivity;
+public class MainActivity extends Activity {
 
-public class MainActivity extends AppCompatActivity {
+    private static final String HOME_URL =
+            "https://biznest.greatindia.technology/";
 
-    private static final String HOME_URL = "https://biznest.greatindia.technology/";
     private WebView webView;
+
     private ValueCallback<Uri[]> fileCallback;
+
     private static final int FILE_CHOOSER_REQUEST = 1001;
+
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
 
         webView = new WebView(this);
+
         setContentView(webView);
 
-        webView.setBackgroundColor(Color.WHITE);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.getSettings().setDomStorageEnabled(true);
-        webView.getSettings().setDatabaseEnabled(true);
-        webView.getSettings().setAllowFileAccess(true);
-        webView.getSettings().setLoadWithOverviewMode(true);
-        webView.getSettings().setUseWideViewPort(true);
-        webView.getSettings().setSupportZoom(false);
 
-        CookieManager.getInstance().setAcceptCookie(true);
-        CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true);
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.getSettings().setDomStorageEnabled(true);
+
+        webView.getSettings().setAllowFileAccess(true);
+
+        webView.getSettings().setLoadWithOverviewMode(true);
+
+        webView.getSettings().setUseWideViewPort(true);
+
+
+        CookieManager cookieManager =
+                CookieManager.getInstance();
+
+        cookieManager.setAcceptCookie(true);
+
+        CookieManager.getInstance()
+                .setAcceptThirdPartyCookies(webView, true);
+
 
         webView.setWebViewClient(new WebViewClient() {
+
             @Override
-            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            public boolean shouldOverrideUrlLoading(
+                    WebView view,
+                    WebResourceRequest request
+            ) {
+
                 Uri uri = request.getUrl();
+
+                String scheme = uri.getScheme();
+
                 String host = uri.getHost();
 
-                if (host != null && host.endsWith("greatindia.technology")) {
-                    view.loadUrl(uri.toString());
-                } else if ("mailto".equals(uri.getScheme()) || "tel".equals(uri.getScheme())) {
-                    startActivity(new Intent(Intent.ACTION_VIEW, uri));
-                } else {
+
+                if ("http".equals(scheme)
+                        || "https".equals(scheme)) {
+
+                    if (host != null
+                            && host.endsWith(
+                            "greatindia.technology"
+                    )) {
+
+                        view.loadUrl(uri.toString());
+
+                    } else {
+
+                        try {
+
+                            Intent intent =
+                                    new Intent(
+                                            Intent.ACTION_VIEW,
+                                            uri
+                                    );
+
+                            startActivity(intent);
+
+                        } catch (Exception ignored) {
+
+                        }
+
+                    }
+
+                    return true;
+
+                }
+
+
+                if ("tel".equals(scheme)
+                        || "mailto".equals(scheme)) {
+
                     try {
-                        startActivity(new Intent(Intent.ACTION_VIEW, uri));
-                    } catch (Exception ignored) {}
+
+                        Intent intent =
+                                new Intent(
+                                        Intent.ACTION_VIEW,
+                                        uri
+                                );
+
+                        startActivity(intent);
+
+                    } catch (Exception ignored) {
+
+                    }
+
+                    return true;
+
                 }
-                return true;
+
+                return false;
+
             }
+
         });
 
-        webView.setWebChromeClient(new WebChromeClient() {
-            @Override
-            public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> callback, FileChooserParams params) {
-                if (fileCallback != null) fileCallback.onReceiveValue(null);
-                fileCallback = callback;
 
-                Intent intent = params.createIntent();
-                try {
-                    startActivityForResult(intent, FILE_CHOOSER_REQUEST);
-                } catch (Exception e) {
-                    fileCallback = null;
-                    return false;
+        webView.setWebChromeClient(
+                new WebChromeClient() {
+
+                    @Override
+                    public boolean onShowFileChooser(
+
+                            WebView view,
+
+                            ValueCallback<Uri[]> callback,
+
+                            FileChooserParams params
+                    ) {
+
+                        if (fileCallback != null) {
+
+                            fileCallback.onReceiveValue(null);
+
+                        }
+
+
+                        fileCallback = callback;
+
+
+                        try {
+
+                            Intent intent =
+                                    params.createIntent();
+
+                            startActivityForResult(
+
+                                    intent,
+
+                                    FILE_CHOOSER_REQUEST
+
+                            );
+
+                            return true;
+
+                        } catch (Exception e) {
+
+                            fileCallback = null;
+
+                            return false;
+
+                        }
+
+                    }
+
                 }
-                return true;
-            }
-        });
+        );
 
-        webView.loadUrl(HOME_URL);
+
+        if (savedInstanceState != null) {
+
+            webView.restoreState(savedInstanceState);
+
+        } else {
+
+            webView.loadUrl(HOME_URL);
+
+        }
+
     }
+
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    protected void onSaveInstanceState(Bundle outState) {
 
-        if (requestCode == FILE_CHOOSER_REQUEST && fileCallback != null) {
-            Uri[] results = WebChromeClient.FileChooserParams.parseResult(resultCode, data);
-            fileCallback.onReceiveValue(results);
-            fileCallback = null;
-        }
+        webView.saveState(outState);
+
+        super.onSaveInstanceState(outState);
+
     }
+
+
+    @Override
+    protected void onActivityResult(
+
+            int requestCode,
+
+            int resultCode,
+
+            Intent data
+    ) {
+
+        super.onActivityResult(
+
+                requestCode,
+
+                resultCode,
+
+                data
+        );
+
+
+        if (requestCode == FILE_CHOOSER_REQUEST
+                && fileCallback != null) {
+
+            Uri[] results =
+                    WebChromeClient.FileChooserParams
+                            .parseResult(
+                                    resultCode,
+                                    data
+                            );
+
+            fileCallback.onReceiveValue(results);
+
+            fileCallback = null;
+
+        }
+
+    }
+
 
     @Override
     public void onBackPressed() {
-        if (webView.canGoBack()) webView.goBack();
-        else super.onBackPressed();
+
+        if (webView.canGoBack()) {
+
+            webView.goBack();
+
+        } else {
+
+            super.onBackPressed();
+
+        }
+
     }
+
 }
